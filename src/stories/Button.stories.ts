5d50379ab9e918eb42933d02f2b6b56988d408be
justL <justlikesuolong@outlook.com>
2024-06-05 14:30:35 +0800

    feat: 引入 tailwind & shadcn ui

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Button } from './Button';
+import { CusButton as Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -20,33 +20,15 @@ const meta = {
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
+// noinspection JSUnusedGlobalSymbols
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+// noinspection JSUnusedGlobalSymbols
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
     label: 'Button',
   },
 };
