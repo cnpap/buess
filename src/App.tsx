10cdfe5b288a309bcd1047490a3468852dfb0286
justL <justlikesuolong@outlook.com>
2024-06-08 18:39:39 +0800

    chore: 主题切换

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,22 @@ import AuthRouteTemplate from '@/routes/auth';
 import '@/globals.css';
 import { withErrorHandler } from '@/error-handling';
 import AppErrorBoundaryFallback from '@/error-handling/fallbacks/App';
+import { useTheme } from './components/theme-provider';
+import { useHotkeys } from 'react-hotkeys-hook';
+
+export function useToggleEvent() {
+  const { setTheme, theme } = useTheme();
+  useHotkeys('alt+t', () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  });
+}
 
 function App() {
+  useToggleEvent();
   return (
     <Fragment>
       <BrowserRouter>
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,7 @@
 import { ComponentType, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -8,9 +9,11 @@ const root = createRoot(container);
 function render(App: ComponentType) {
   root.render(
     <StrictMode>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </ThemeProvider>
     </StrictMode>,
   );
 }
