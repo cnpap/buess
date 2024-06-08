80d30de8496f6f6be8f31a2adf0c597899d3f917
justL <justlikesuolong@outlook.com>
2024-06-08 20:55:16 +0800

    fix: external: ['tslib'] 引发测试环境报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,40 @@
-/// <reference types="vitest" />
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-
 import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest,
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      // switch to "true" to enable sw on development
-      devOptions: {
-        enabled: false,
+// noinspection JSUnusedGlobalSymbols
+export default defineConfig(() => {
+  return ({
+    plugins: [
+      react(),
+      VitePWA({
+        manifest,
+        includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+        // switch to "true" to enable sw on development
+        devOptions: {
+          enabled: false,
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'],
-      },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      external: ['tslib']
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
     },
-  },
-  test: {
-    root: path.resolve(__dirname, './src'),
-  },
-  optimizeDeps: {
-    include: [
-      '@storybook/blocks',
-      '@mdx-js/react'
-    ]
-  }
+    test: {
+      root: path.resolve(__dirname, './src'),
+    },
+    optimizeDeps: {
+      include: [
+        '@storybook/blocks',
+        '@mdx-js/react'
+      ]
+    }
+  })
 });
