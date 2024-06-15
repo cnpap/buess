1e020dd40c67e4c18cfeb8d6d5e1b393cc6fcce8
justL <justlikesuolong@outlook.com>
2024-06-15 15:43:09 +0800

    style: 微调登录布局

--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -3,20 +3,25 @@ import { Outlet } from 'react-router-dom';
 export default function AuthLayout() {
   // noinspection HtmlUnknownTarget
   return (
-    <div className="w-full lg:grid lg:grid-cols-2 h-full">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
+    <div
+      className="
+      w-full lg:grid lg:grid-cols-2 h-fullabsolute inset-0 z-0 h-full bg-white dark:bg-[#090909]
+      bg-[linear-gradient(to_right,#f9f9f9_1px,transparent_1px),linear-gradient(to_bottom,#f9f9f9_1px,transparent_1px)] bg-[size:10rem_10rem]
+      dark:bg-[linear-gradient(to_right,#090909_1px,transparent_1px),linear-gradient(to_bottom,#090909_1px,transparent_1px)]
+      [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)
+      "
+    >
+      <div className="z-20 flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[300px] gap-6">
           <Outlet />
         </div>
       </div>
-      <div className="hidden bg-muted lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+      <div
+        className="
+      hidden lg:block
+      "
+      >
+        <img src="/auth/background.png" alt="Image" className="h-screen w-screen" />
       </div>
     </div>
   );
