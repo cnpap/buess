9158514d937a09730a797aea1b01770e20fd4ecc
justL <justlikesuolong@outlook.com>
2024-06-18 10:49:18 +0800

    chore: 调整 seed 脚本

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 // noinspection JSIgnoredPromiseFromCall
 
 import { faker } from '@faker-js/faker';
-import { UserStatus, InvitationStatus, OrganizationRole, Prisma } from '@prisma/client';
+import { UserStatus, InvitationStatus, OrganizationRole, MemberRole, Prisma } from '@prisma/client';
 import { hash } from 'argon2';
 import { defaultPassword, primaryUserEmail } from './const';
 import { maPrisma } from '../src/utils/facade-init';
@@ -91,20 +91,47 @@ async function createOrganization() {
   });
 }
 
-async function createOrganizationMembers() {
-  await prisma.organizationMembers.deleteMany({});
+let primaryOrgId: string;
+let primaryUserId: string;
+
+async function getPrimaryOrgId() {
   const primaryOrg = await prisma.organization.findFirst({
     where: {
       name: 'Sia',
     },
   });
+
+  if (!primaryOrg) {
+    throw new Error('Primary organization not found');
+  }
+
+  primaryOrgId = primaryOrg.id;
+
+  return primaryOrgId;
+}
+
+async function getPrimaryUserId() {
   const primaryUser = await prisma.user.findFirst({
     where: {
       email: primaryUserEmail,
     },
   });
 
-  if (!primaryOrg || !primaryUser) {
+  if (!primaryUser) {
+    throw new Error('Primary user not found');
+  }
+
+  primaryUserId = primaryUser.id;
+
+  return primaryUserId;
+}
+
+async function createOrganizationMembers() {
+  await prisma.organizationMembers.deleteMany({});
+  const primaryOrgId = await getPrimaryOrgId();
+  const primaryUserId = await getPrimaryUserId();
+
+  if (!primaryOrgId || !primaryUserId) {
     throw new Error('Primary organization or user not found');
   }
 
@@ -116,9 +143,88 @@ async function createOrganizationMembers() {
       role: OrganizationRole.ADMIN,
       status: InvitationStatus.ACCEPTED,
 
-      organizationId: primaryOrg.id,
-      userId: primaryUser.id,
+      organizationId: primaryOrgId,
+      userId: primaryUserId,
     },
+  });
+}
+
+async function createProject() {
+  await prisma.project.deleteMany({});
+  const primaryOrgId = await getPrimaryOrgId();
+
+  function gen(): Prisma.ProjectCreateManyInput {
+    return {
+      name: faker.company.name(),
+      cover: faker.image.avatar(),
+      desc: faker.lorem.sentence(),
+
+      createdAt: new Date(),
+      updatedAt: new Date(),
+
+      organizationId: primaryOrgId
+    };
+  }
+
+  const projects = Array.from({ length: 10 }, gen);
+
+  await prisma.project.createMany({
+    data: projects,
+  });
+}
+
+async function createTags() {
+  await prisma.tag.deleteMany({});
+  const projectId = await getPrimaryOrgId();
+
+  function gen(): Prisma.TagCreateManyInput {
+    return {
+      name: faker.lorem.word(),
+      color: faker.internet.color(),
+      projectId,
+    };
+  }
+
+  const tags = Array.from({ length: 10 }, gen);
+
+  await prisma.tag.createMany({
+    data: tags,
+  });
+}
+
+async function createProjectMembers() {
+  await prisma.members.deleteMany({});
+  const primaryOrgId = await getPrimaryOrgId();
+  const primaryUserId = await getPrimaryUserId();
+
+  if (!primaryOrgId || !primaryUserId) {
+    throw new Error('Primary organization or user not found');
+  }
+
+  const projects = await prisma.project.findMany({
+    where: {
+      organizationId: primaryOrgId,
+    },
+  });
+
+  if (!projects) {
+    throw new Error('Projects not found');
+  }
+
+  const projectMembers: Prisma.MembersCreateManyInput[] = projects.map(project => {
+    return {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+
+      role: MemberRole.CREATOR,
+
+      projectId: project.id,
+      userId: primaryUserId,
+    };
+  });
+
+  await prisma.members.createMany({
+    data: projectMembers,
   });
 }
 
@@ -126,6 +232,9 @@ async function _do() {
   await createUsers();
   await createOrganization();
   await createOrganizationMembers();
+  await createProject();
+  await createProjectMembers();
+  await createTags();
 }
 
 _do();
