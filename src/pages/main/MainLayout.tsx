1b9ae551779f02dbdf80c35f79f9deb87f818e54
justL <justlikesuolong@outlook.com>
2024-06-18 09:49:36 +0800

    style: 抽离 header 到 layout

--- src/pages/main/MainLayout.tsx
+++ src/pages/main/MainLayout.tsx
@@ -1,11 +1,13 @@
 import Aside from '@/pages/main/Aside';
 import { Outlet } from 'react-router-dom';
+import Header from '@/pages/main/Header';
 
 export default function Dashboard() {
   return (
     <div className="grid h-screen w-full pl-[5rem]">
       <Aside />
-      <main className="flex-1 overflow-auto md:grid-cols-2 lg:grid-cols-3">
+      <Header />
+      <main className="h-[calc(100vh-var(--header-height))] flex-1 overflow-auto md:grid-cols-2 lg:grid-cols-3">
         <Outlet />
       </main>
     </div>
