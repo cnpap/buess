fba7a06789bbed8af08827d4a585083e8abe8bb4
justL <justlikesuolong@outlook.com>
2024-06-26 17:48:52 +0800

    pref: 跟进 viteser 版本 & 支持 vercel 部署

--- src/services/func.ts
+++ src/services/func.ts
@@ -1,16 +1,16 @@
-import * as process from 'node:process';
+import { env } from 'node:process';
 import jwt from 'jsonwebtoken';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function genToken(payload: Record<string, any>) {
-  const secret = process.env.JWT_SECRET ?? 'secret';
+  const secret = env.JWT_SECRET ?? 'secret';
   return jwt.sign(payload, secret, {
     expiresIn: '3h',
   });
 }
 
 export async function verifyToken(token: string) {
-  const secret = process.env.JWT_SECRET ?? 'secret';
+  const secret = env.JWT_SECRET ?? 'secret';
   await new Promise((resolve, reject) => {
     jwt.verify(token, secret, (error, decoded) => {
       if (error) {
