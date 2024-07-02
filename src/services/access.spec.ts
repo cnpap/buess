59b2088652e60040182eafaafda3a4c3bc65bd3f
justL <justlikesuolong@outlook.com>
2024-07-02 23:11:39 +0800

    ci: 补充环境变量 & 打印失败原因

--- src/services/access.spec.ts
+++ src/services/access.spec.ts
@@ -5,6 +5,9 @@ describe('test access service', () => {
   it('test fresh token', async () => {
     const result = await fetchAccessToken();
     const status = result.status;
+    if (status !== 200) {
+      console.error(await result.text());
+    }
     expect(status).toBe(200);
     const json = await result.json();
     expect(json.access_token).toBeDefined();
