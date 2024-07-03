32243efd97e1d6284ed9defe483177f0c2ea1586
justL <justlikesuolong@outlook.com>
2024-07-03 10:57:24 +0800

    chore: 清理不用的图片文件

--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -20,19 +20,15 @@ export default function AuthLayout() {
   return (
     <div
       className="
-      w-full lg:grid lg:grid-cols-2 h-fullabsolute inset-0 z-0 h-full bg-white dark:bg-[#090909]
+      w-full absolute inset-0 z-0 h-full bg-white dark:bg-[#090909]
       bg-[linear-gradient(to_right,#f9f9f9_1px,transparent_1px),linear-gradient(to_bottom,#f9f9f9_1px,transparent_1px)] bg-[size:10rem_10rem]
       dark:bg-[linear-gradient(to_right,#090909_1px,transparent_1px),linear-gradient(to_bottom,#090909_1px,transparent_1px)]
       [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)
       "
     >
-      <div className="z-20 flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[300px] gap-6">
-          <div className={'flex justify-center'}>
-            <img src={'/logo.png'} style={{ width: '200px' }} alt="logo" />
-          </div>
-          <Button onClick={() => signIn(`${VITE_BASE_URL}/callback`)}>Sign In</Button>
-        </div>
+      {/*绝对定位，绝对居中*/}
+      <div className="absolute inset-0 flex items-center justify-center z-10 min-w-[40rem]">
+        <Button onClick={() => signIn(`${VITE_BASE_URL}/callback`)}>To sign in page</Button>
       </div>
       <div
         className="
