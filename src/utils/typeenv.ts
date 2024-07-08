c72bb8811bf9db794439abfb7975c3242686c184
justL <justlikesuolong@outlook.com>
2024-07-09 04:31:12 +0800

    pref: 优化环境变量静态化脚本

--- src/utils/typeenv.ts
+++ src/utils/typeenv.ts
@@ -7,23 +7,17 @@ import * as process from 'node:process';
 dotenv.config();
 
 const env = process.env;
-const envVariables = Object.keys(env)
-  .filter((key) => {
-    if (key.startsWith('VITE_') || ['MONGODB_URL'].includes(key)) {
-      console.log(`Exporting ${key}`);
-      return true;
-    }
-  })
-  .map((key) => `export const ${key} = ${JSON.stringify(env[key])};`)
-  .join('\n');
-
 const outputDir = path.resolve(process.cwd(), 'src');
 const outputFilePath = path.join(outputDir, 'const.ts');
 
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir, { recursive: true });
-}
+const envVariables = Object.keys(env).reduce((acc, key) => {
+  if (key.startsWith('VITE_') || ['MONGODB_URL'].includes(key)) {
+    const variableString = `export const ${key} = ${JSON.stringify(env[key])};\n`;
+    console.log(`Exporting ${key}`);
+    return acc + variableString;
+  }
+  return acc;
+}, '');
 
 fs.writeFileSync(outputFilePath, envVariables);
-
 console.log(`Environment constants generated at ${outputFilePath}`);
